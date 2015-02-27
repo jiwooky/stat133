add8b9c9652732610071eb4d307486d78c0e38ba
Jiwook Yoo <jiwooky@berkeley.edu>
2015-02-26 18:50:59 -0800

    hw4

--- assignments/hw4/hw4.r
+++ assignments/hw4/hw4.r
@@ -12,13 +12,11 @@
 # Output variable:
 # <element.lengths>: a numeric vector whose entries are the lengths of each
 #   element of <data.list>
-
+data.list = list(x = c(1:10), y = c(1:5))
 listLengths <- function(data.list) {
-
-    # your code here
-
+return(lapply(data.list, length))
 }
-
+listLengths(data.list)
 #### Function 2
 #### Implement the function "powers"
 
@@ -29,15 +27,23 @@ listLengths <- function(data.list) {
 # Output variable
 # <x.powers> : A matrix of size [n x k] where the first column is x, the second column x^2, the third column x^4, etc.
 #              the column names should be : "x", "x^2", "x^3" etc.
-
+x = c(1:5)
+k = 5
 powers <- function(x, k){
-
+x.powers = matrix(x , nrow = length(x), ncol = k, byrow = FALSE, dimnames = NULL)
+colnames(x.powers) = colnames(x.powers, do.NULL = FALSE, prefix = "x^")
+for(a in 1:length(x)){
+  for(b in 1:k){
+    x.powers[a,b] = a^b
+  }
 }
-
- 
+}
+powers(x, k)
+x.powers[4,2]
 #### Function #3
 #### Implement the function "recipeConversion"
-
+recipe = data.frame(amount = (1:4), unit = c("cups", "oz", "pounds", "oz"), 
+                    ingredient = c("flour", "eggs", "sugar", "honey"), stringsAsFactors = FALSE)
 # Input variable:
 # <recipe> : A data frame with three columns named "amount", "unit" and "ingredient"
 
@@ -64,9 +70,20 @@ powers <- function(x, k){
 
 # Put your code here
 recipeConversion <- function(recipe){
-
+cups = grep("cup", "cups", recipe$unit, ignore.case = TRUE)
+oz = grep("oz", recipe$unit, ignore.case = TRUE)
+for(e in cups){
+  recipe$amount[e] = 236.6*recipe$amount[e]
+  recipe$unit[e] = c("ml")
 }
-
+for(d in oz){
+  recipe$amount[d] = 28.3*recipe$amount[d]
+  recipe$unit[d] <- "grams"
+}
+recipe.metric = recipe
+return(recipe.metric)
+}
+metric = recipeConversion(recipe)
 
 #### Function #4a
 # Implement the function "bootstrapVarEst"
@@ -88,11 +105,19 @@ recipeConversion <- function(recipe){
 #    i.e. we draw n values at random *with replacement* from the observations X_1, ..., X_n. (use sample())
 # -- Calculate, and store, the mean of this bootstrap sample, call that mu_i (i in 1:B)
 # -- The bootstrap variance is the sample variance of mu_1, mu_2, ..., mu_B
-
+x = 1:100
+B = 10000
+hold = vector('numeric')
 bootstrapVarEst <- function(x, B){
-
+for(p in 1:B){
+  sample = sample(x, size = 100, replace = TRUE, prob = NULL)
+  mu_i = mean(sample)
+  hold = c(hold, mu_i)
 }
-
+boot.sigma2.est = (sd(hold))^2
+return(boot.sigma2.est)
+}
+bootstrap = bootstrapVarEst(x,B)
 #### Function #4b
 #### Implement the function "jackknifeVarEst"
 
@@ -107,14 +132,21 @@ bootstrapVarEst <- function(x, B){
 # of the distrubution of the random variables X, and we want an estimate of the variance of that 
 # sample mean (note, not the variance of the X_i but the variance of the sample mean).
 # We do this by:
-# -- For each i in 1:n we omit the i-th observation (i.e. take the sample X_1, ..., X_[i-1], X_[i+1], ..., X_n)
+# -- we omit the i-th observation (i.e. take the sample X_1, ..., X_[i-1], X_[i+1], ..., X_n)
 #     for this reduced sample calculate the sample mean (get mu_1, mu_2, ..., mu_n)
 # -- The jackknife variance is the sample variance of mu_1, mu_2, ..., mu_n
-
-jackknifeVarEst <- fuction(x){
-
+hold2 = vector('numeric')
+jackknifeVarEst <- function(x){
+for(p in 1:100){
+  sample = sample(x, size = 100, replace = FALSE, prob = NULL)
+  mu_i = sample
+  mu_i = mu_i[-p]
+  hold2 = c(hold2, mean(mu_i))
 }
-
+  jack.sigma2.est = (sd(hold2))^2
+  return(jack.sigma2.est)
+}
+jack = jackknifeVarEst(x)
 #### Function #4c
 #### Implement the function "samplingVarEst"
 
@@ -126,9 +158,19 @@ jackknifeVarEst <- fuction(x){
 # <sampling.sigma.est> : The bootstrap estimate if type="bootstrap" and the jackknife estimate if type="jackknife"
 
 # Note: this function calls the previous two functions.
-
-samplingVarEst <- function(  ){
-
+samplingVarEst <- function(x, type){
+if(type == "bootstrap"){
+  var = bootstrapVarEst(x,B)
 }
-
+if(type == "jackknife")
+{
+  var = jackknifeVarEst(x)
+}
+else
+{
+  var = bootstrapVarEst(x,B)
+}
+return(var)
+}
+aa= samplingVarEst(x, bootstrap)
 
